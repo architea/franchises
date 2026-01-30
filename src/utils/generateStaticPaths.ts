3705a99ea3d8339c7689cb446f5e6d7a89f50169
huitquatredev <julien@huitquatre.fr>
2026-01-30 11:43:21 +0100

    commit

--- src/utils/generateStaticPaths.ts
+++ src/utils/generateStaticPaths.ts
@@ -3,19 +3,20 @@ import isPreview from "./isPreview.js"
 
 export default async function generateStaticPaths() {
   const response = await storyblokFetch("cdn/stories", {
-    version: isPreview() ? "draft" : "published"
+    version: isPreview() ? "draft" : "published",
+    per_page: 100
   })
-  
+
   const paths = response.data.stories
     .filter((link: any) => !link.full_slug.startsWith("actualites"))
     .filter((link: any) => link.full_slug !== "global")
     .map((link: any) => {
-      const slug = link.full_slug === "home" ? undefined : link.full_slug  // undefined for home!
+      const slug = link.full_slug === "home" ? undefined : link.full_slug // undefined for home!
       return {
         params: { path: slug },
         props: { path: slug, name: link.name }
       }
     })
-  
+
   return paths
 }
